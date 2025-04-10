993756074e6a4f39ccc1682cbde4bd33374e135f
Duc Vu <vu.254@buckeyemail.osu.edu>
2025-04-10 17:07:43 -0400

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,4 @@
+import open_clip
+
+model, preprocess_train, preprocess_val = open_clip.create_model_and_transforms('hf-hub:imageomics/bioclip')
+tokenizer = open_clip.get_tokenizer('hf-hub:imageomics/bioclip')
